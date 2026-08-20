7a69a764448b75a622bef212ad07faa1bde7bb1c
Iren <irene19098@gmail.com>
2026-08-20 23:16:52 +0300

    main: Первые запросы

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(ktorLibs.plugins.ktor)
+    kotlin("plugin.serialization") version "2.4.0"
 }
 
 group = "com.irklch.message"
@@ -17,6 +18,8 @@ dependencies {
     implementation(ktorLibs.server.config.yaml)
     implementation(ktorLibs.server.core)
     implementation(ktorLibs.server.netty)
+    implementation("io.ktor:ktor-server-content-negotiation:3.5.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.5.0")
     implementation(libs.logback.classic)
 
     testImplementation(kotlin("test"))
--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,13 +1,32 @@
 package com.irklch.message
 
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import java.util.concurrent.atomic.AtomicReference
+
+@Serializable
+data class MessageResponse(val text: String)
+
+private var sharedText = AtomicReference("Привет! Это текстовое сообщение")
 
 fun Application.configureRouting() {
+    install(ContentNegotiation) {
+        json()
+    }
     routing {
-        get("/") {
-            call.respondText("Hello, World!")
+        get("/message") {
+            call.respond(MessageResponse(sharedText.get()))
+        }
+
+        put("/message") {
+           val newMessage = call.receive<MessageResponse>()
+            sharedText.set(newMessage.text)
+            call.respond(MessageResponse(sharedText.get()))
         }
     }
 }
